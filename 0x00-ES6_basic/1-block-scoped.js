1904cb4603ce479ad4dc67fd2e8098b421e058d9
Dahesey <dahesey@gmail.com>
2024-07-24 00:18:10 +0000

    A script to prevent variables from being overriden inside a conditional block

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+"use strict"
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+
+    if (trueOrFalse) {
+        let task = true;
+        let task2 = false;
+        }
+        return [task, task2];
+        }
