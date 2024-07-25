388db42eb2b3ebee544151b3d701bd3650361891
Dahesey <dahesey@gmail.com>
2024-07-25 11:09:00 +0000

    ES6 loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const modifiedArray = [];
+  for (const idx of array) {
+    modifiedArray.push(appendString + idx);
+  }
+  return modifiedArray;
+}
